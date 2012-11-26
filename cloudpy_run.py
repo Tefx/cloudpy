400474b0f7d6a7a8122cf1e60e6f25ecdbe65778
Teƒx <zhaomeng.zhu@gmail.com>
2012-11-26 21:03:17 +0800

    0.091

--- cloudpy_run.py
+++ cloudpy_run.py
@@ -1,69 +1,33 @@
-from cloudpy_struct import DirStruct
+#!/usr/bin/env python
+
 import sh
-import os
+from cloudpy_config import Config
 import sys
+import argparse
+from sys import argv
 
 
-class Agent(object):
-    def __init__(self, path):
-        self.dir = DirStruct(path)
-        self.mods = []
-        self.conf = {}
-        self.read_mods()
-        self.read_conf()
-        self.working_dir = self.dir.append("SRC_DIR", self.conf["PWD"])
-        self.mainfile = self.dir.append("SRC_DIR", self.conf["EXEC"])
+config = Config()
+eval_prog = "cloudpy_eval.py"
 
-    def read_mods(self):
-        with open(self.dir.MODS_FILE, "r") as f:
-            for line in f:
-                line = line.strip()
-                if line and not line.startswith("#"):
-                    items = line.split()
-                    if len(items) == 1:
-                        self.mods.append(items[0])
-                    else:
-                        self.mods.append("==".join(items))
+parser = argparse.ArgumentParser(description='Run cloudpy package remotely.')
+parser.add_argument("-H", "--host", action='store')
+parser.add_argument("package", help="Cloudpy package name")
+args = parser.parse_args(argv[1:])
 
-    def read_conf(self):
-        with open(self.dir.CONF_FILE, "r") as f:
-            for line in f:
-                line = line.strip()
-                if line and not line.startswith("#"):
-                    attr, value = line.split("=")
-                    self.conf[attr.strip()] = value.strip()
+if args.host:
+    host = args.host
+else:
+    host = config.host
 
+eval_cmd = config.host_sep.join([config.host_bin, eval_prog])
+target = config.host_sep.join([config.depository, args.package])
 
-    def setup_env(self, quiet=False):
-        if quiet:
-            outproc = lambda x:None
-        else:
-            outproc = self.print_output
-        env_dir = self.dir.ENV_DIR
-        if not os.path.exists(env_dir):
-            sh.virtualenv(env_dir, _out=outproc).wait()
-        activate_this = self.dir.append("ENV_DIR",'bin/activate_this.py')
-        execfile(activate_this, dict(__file__=activate_this))
-        sh.pip.install(self.mods, _out=outproc).wait()
+def print_line(line):
+    sys.stdout.write(line)
+    sys.stdout.flush()
 
-    def print_output(self, line):
-        sys.stdout.write(line)
-
-    def run(self):
-        sh.cd(self.working_dir)
-        sh.python(self.mainfile, _out=self.print_output).wait()
+sh.ssh("-t", host, "python", eval_cmd, target, _out=print_line, _tty_in=True).wait()
 
 
 
-if __name__ == '__main__':
-    from sys import argv
-    import argparse
-
-    parser = argparse.ArgumentParser(description='run python script in a virtual environment.')
-    parser.add_argument("package", help="Package Top Dir Location")
-    parser.add_argument("-q", "--quiet", action='store_true')
-    args = parser.parse_args(argv[1:])
-
-    agent = Agent(args.package)
-    agent.setup_env(args.quiet)
-    agent.run()
